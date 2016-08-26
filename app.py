957a930d877bab94d2789d442422ecc7c2bfd5ac
Daniel Cripps <dancripps.dc@gmail.com>
2016-08-26 13:03:30 +0100

    Error Fixes
    Fixed syntax errors in app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import render_template
-import database.py as db
+import database as db
 
 app = Flask(__name__)
 
@@ -9,8 +9,8 @@ app = Flask(__name__)
 def main():
     return render_template('main.html',recipe={'name':'Spaghetti'})
 
-@app.route('/recipes/<int:recipe_id>')
-def recipe():
+@app.route('/recipes/<recipe_id>')
+def recipe(recipe_id):
     return render_template('recipe.html',recipe=db.getRecipe(recipe_id))
 
 if __name__ == '__main__':
