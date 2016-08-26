970d182818b5a74d13201f6d7c386d4f48360903
Daniel Cripps <dancripps.dc@gmail.com>
2016-08-26 13:05:12 +0100

    Added basic database recipe functions
    Commented out testing code and added getRecipe() and AddRecipe()
    functions

--- database.py
+++ database.py
@@ -1,23 +1,38 @@
 from pymongo import MongoClient
 import datetime
+import sys
 
 client = MongoClient()
 db = client.allspice
 recipes = db.recipes
 
-recipe = {
-    'name':'Spaghetti',
-    'description':'A wonderful spaghetti recipe',
-    'ingredients':['spaghetti','sauce','basil'],
-    'steps':['cook spaghetti','add sauce','add basil'],
-    'tags':['pasta','quick','easy'],
-    'notes':'Remember the spaghetti',
-    'date': datetime.datetime.utcnow()
-}
+# recipe = {
+#     'name':'Spaghetti',
+#     'description':'A wonderful spaghetti recipe',
+#     'ingredients':['spaghetti','sauce','basil'],
+#     'steps':['cook spaghetti','add sauce','add basil'],
+#     'tags':['pasta','quick','easy'],
+#     'notes':'Remember the spaghetti',
+#     'date': datetime.datetime.utcnow()
+# }
+# recipes.insert_one(recipe)
+# print(recipes.find_one())
 
-recipes.insert_one(recipe)
+def getRecipe(rid):
+    # IDEA: Add more query options beyond just _id
+    try:
+        recipe = recipes.find_one({'_id': ObjectId(rid)})
+        return recipe
+    except:
+        print(sys.exc_info()[0])
 
-print(recipes.find_one())
-
-def getRecipe():
-    pass
+def AddRecipe(name, description, ingredients, steps, tags, notes):
+    r = {
+        'name':name,
+        'description':description,
+        'ingredients':ingredients,
+        'steps':steps,
+        'tags':tags,
+        'notes':notes
+    }
+    recipes.insert_one(r)
